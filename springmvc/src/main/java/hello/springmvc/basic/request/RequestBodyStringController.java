1973027f89e9755ebca0b0ddfc7618a6119df580
AidenRoh <colohrby@gmail.com>
2023-08-14 23:43:09 +0900

    230814

---
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -0,0 +1,69 @@
+package hello.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+@Controller
+public class RequestBodyStringController {
+
+    @PostMapping("/request-body-string-v1")
+    public void requestBodyString(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody={}", messageBody);
+
+        response.getWriter().write("ok");
+    }
+
+    /**
+     *
+     * @param inputStream InputStream(Reader) : Http 요청 메시지 바디의 내용일 직접 조회
+     * @param responseWriter OutputStream(Writer) : Http 응답 메시지의 바디에 직접 결과 출력
+     * @throws IOException
+     */
+    @PostMapping("/request-body-string-v2")
+    public void requestBodyStringV2(InputStream inputStream, Writer responseWriter) throws IOException {
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody={}", messageBody);
+
+        responseWriter.write("ok");
+    }
+
+    @PostMapping("/request-body-string-v3")
+    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) throws IOException {
+        String messageBody = httpEntity.getBody();
+        log.info("messageBody={}", messageBody);
+
+        return new HttpEntity<>("ok");
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-string-v4")
+    public String requestBodyStringV4(@RequestBody String messageBody) {
+        log.info("messageBody={}", messageBody);
+
+        return "ok";
+
+        /**
+         * 요청 파라미터를 다룰 때: @RequestParam, @MedelAttribute
+         * HTTP 메시지 바디를 직접 조회하고 싶을 땐: @RequestBody
+         * HTTP 메시지 바디에 직접 담아서 전달할 땐: @ResponseBody
+         */
+    }
+}
